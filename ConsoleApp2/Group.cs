71866e600b6aa00ac47d6033bd4d85f310566c83
ShamilHajiyev <shamilnh@code.edu.az>
2022-04-25 19:31:48 +0400

    initial commit

---
+++ ConsoleApp2/Group.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class Group
+    {
+        public string No;
+        public byte Limit;
+        private Student[] StudentGroup;
+
+        public Group(string no, byte limit)
+        {
+            Limit = limit;
+            No = no;
+            StudentGroup = new Student[0];
+        }
+
+        public void AddStudent(Student addedStudent)
+        {
+            if (StudentGroup.Length <= Limit)
+            {
+                Array.Resize(ref StudentGroup, StudentGroup.Length + 1);
+                StudentGroup[StudentGroup.Length - 1] = addedStudent;
+                Console.WriteLine($"{addedStudent.Name} added to {No}");
+            }
+            else
+            {
+                Console.WriteLine("limit reached");
+            }
+        }
+
+        public void GetStudents()
+        {
+            foreach (Student student in StudentGroup)
+            {
+                Console.WriteLine($"{student.Name} {student.Surname}");
+            }
+        }
+
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the name and surname of the added student:");
+            string name = Console.ReadLine();
+            string surname = Console.ReadLine();
+            Student student = new Student(name, surname);
+            Group group = new Group("P324", 5);
+            group.AddStudent(student);
+            group.GetStudents();
+        }
+    }
+}
